10c17848d936857c3eb8ce76dee10c0756762d42
AnkushMishra2609 <mishra.ankush2609@gmail.com>
2025-05-01 11:47:22 +0530

    Commiting the starter code installation and packags

---
+++ frontend/morning_news/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+     <h1 className="text-5xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App

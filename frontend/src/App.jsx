564ad046290f6ebea7b25ed452644e9314e742b9
AnkushMishra2609 <mishra.ankush2609@gmail.com>
2025-05-02 10:04:12 +0530

    Commiting sadcn Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { Button } from "@/components/ui/button"
 const App = () => {
   return (
     <>
      <h1 className="text-5xl font-bold underline text-red-500">
       Hello world!
     </h1>
+    <Button className="bg-red-300" variant="ghost">Click me</Button>
     </>
   )
 }

72f57b51f8a4cbced6f63f539bf82184cbf62672
AnkushMishra2609 <mishra.ankush2609@gmail.com>
2025-05-03 10:41:06 +0530

    Commitin the chnages of adding mongooese and connect daqtabase

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from "express"
-const app =express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("DataBase Connet Succesefully")
+}).catch((err)=>{
+console.log(err)
+})
+const app =express();
 app.listen(3000,()=>{
     console.log("The server is up on 3000!!")
 })
